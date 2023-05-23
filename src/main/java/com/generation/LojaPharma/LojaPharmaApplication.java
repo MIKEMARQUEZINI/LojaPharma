1f5d4a5f43cda2934026ce136bc0790fe75c0a69
MIKE <marquezini.mike@outlook.com>
2023-05-23 10:25:50 -0300

    Projeto Farmacia

---
+++ src/main/java/com/generation/LojaPharma/LojaPharmaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.LojaPharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaPharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaPharmaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/generation/LojaPharma/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.generation.LojaPharma.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.LojaPharma.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+	public List <Categoria> findAllByNomeContainingIgnoreCase(@Param("tipo") String nome);
+
+}
